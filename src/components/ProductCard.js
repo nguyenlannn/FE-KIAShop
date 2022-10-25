d714078b2ab506513de4b1f3140719046eb4d3b6
cuongnh2k <cuongnh2k@gmail.com>
2022-10-26 00:29:03 +0700

    fix slide and productCart

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,7 +3,9 @@ import moment from "moment";
 
 const ProductCard = (props) => {
     return <Link to={`/product-detail?id=${props.product.id}`}>
-        <img src={props.product.productImages[0].path} className="card-img-top" alt={props.product.name}/>
+        <img
+            src={props.product.productImages !== null && props.product.productImages.length !== 0 ? props.product.productImages[0].path : 'https://upload.wikimedia.org/wikipedia/commons/c/c3/Yen_Bai_-_dogs_-_P1390010.JPG'}
+            className="card-img-top" alt={props.product.name}/>
         <div className="card-body card">
             <h5 className="card-title text-truncate">{props.product.name}</h5>
             <p className="card-text text-center text-truncate text-secondary">
